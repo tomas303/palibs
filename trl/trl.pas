83449e8764e2d3c1c1f403e2bd69d20dfce283e5
tvr <tvr@post.cz>
2015-12-30 19:29:28 +0100

    add ICryptic interface

--- trl/trl.pas
+++ trl/trl.pas
@@ -9,7 +9,7 @@ interface
 uses
   trl_dicontainer, trl_irttibroker, trl_urttibroker, trl_ipersist, 
   trl_upersistxml, trl_ifactory, trl_upersist, trl_upersiststore, 
-  LazarusPackageIntf;
+  trl_icryptic, LazarusPackageIntf;
 
 implementation
 
---
+++ trl/trl_icryptic.pas
@@ -0,0 +1,26 @@
+unit trl_icryptic;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+
+  { ICryptic }
+
+  ICryptic = interface
+  ['{DC755129-2DA8-482D-A359-D9DB43D54DA2}']
+    function Decode(const AData: String): String;
+    function Encode(const AData: String): String;
+    function GetKey: string;
+    procedure SetKey(AValue: string);
+    property Key: string read GetKey write SetKey;
+  end;
+
+implementation
+
+end.
+
