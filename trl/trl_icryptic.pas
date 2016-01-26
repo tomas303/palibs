27d8d35cfb3029d78841a316f61613f0f6e1746d
tvr <tvr@post.cz>
2016-01-26 19:59:09 +0100

    extend cryptic interface with streams

--- trl/trl_icryptic.pas
+++ trl/trl_icryptic.pas
@@ -15,6 +15,8 @@ type
   ['{DC755129-2DA8-482D-A359-D9DB43D54DA2}']
     function Decode(const AData: String): String;
     function Encode(const AData: String): String;
+    procedure Decode(AInStream, AOutStream: TStream);
+    procedure Encode(AInStream, AOutStream: TStream);
     function GetKey: string;
     procedure SetKey(AValue: string);
     property Key: string read GetKey write SetKey;
