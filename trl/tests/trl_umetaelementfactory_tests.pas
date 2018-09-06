e72531eae2ee3cfe42d13d86cc0c3ed11f08d86b
tvr <tvr@post.cz>
2018-09-06 19:53:53 +0200

    add tests for TMetaElementFactory

---
+++ trl/tests/trl_umetaelementfactory_tests.pas
@@ -0,0 +1,201 @@
+unit trl_umetaelementfactory_tests;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  TestFramework, uDICTestCase, SysUtils,
+  trl_imetaelementfactory, trl_umetaelementfactory,
+  trl_imetaelement, trl_umetaelement,
+  trl_itree, trl_utree,
+  trl_dicontainer,
+  trl_iprops, trl_uprops;
+
+  //trl_ilog,
+  //trl_dicontainer,
+  //trl_itree, trl_utree,
+  //trl_iinjector, trl_uinjector,
+  //;
+
+type
+
+  IDummyObject = interface
+  ['{70D5A48E-1E40-450E-B410-F170B05B83A0}']
+  end;
+
+  TDummyObject = class(TInterfacedObject, IDummyObject)
+  end;
+
+  { TMetaElementFactoryTests }
+
+  TMetaElementFactoryTests = class(TDICTestCase)
+  protected const
+    cTypeID = 'DummyID';
+  protected
+    fMetaElementFactory: IMetaElementFactory;
+    procedure SetUp; override;
+    procedure TearDown; override;
+    procedure CheckMetaElement(const AElement: IMetaElement; AGuid: TGuid;
+      const AID: string; const AProps: IProps; const AChildren: array of IMetaElement);
+  published
+    procedure TestCreateElement_1;
+    procedure TestCreateElement_2;
+    procedure TestCreateElement_3;
+    procedure TestCreateElement_4;
+    procedure TestCreateElement_5;
+    procedure TestCreateElement_6;
+    procedure TestCreateElement_7;
+    procedure TestCreateElement_8;
+  end;
+
+procedure RegisterTests;
+
+implementation
+
+procedure RegisterTests;
+begin
+  TestFramework.RegisterTest(TMetaElementFactoryTests.Suite);
+end;
+
+{ TMetaElementFactoryTests }
+
+procedure TMetaElementFactoryTests.SetUp;
+var
+  mReg: TDIReg;
+begin
+  inherited SetUp;
+  DIC.Add(TParentNode, INode, 'parent');
+  mReg := DIC.Add(TMetaElement, IMetaElement);
+  mReg.InjectProp('Node', INode, 'parent');
+  DIC.Add(TDummyObject, IDummyObject);
+  mReg := DIC.Add(TMetaElementFactory, IMetaElementFactory);
+  mReg.InjectProp('Container', TDIContainer, '', DIC);
+  fMetaElementFactory := IMetaElementFactory(DIC.Locate(IMetaElementFactory));
+end;
+
+procedure TMetaElementFactoryTests.TearDown;
+begin
+  fMetaElementFactory := nil;
+  inherited TearDown;
+end;
+
+procedure TMetaElementFactoryTests.CheckMetaElement(const AElement: IMetaElement;
+  AGuid: TGuid; const AID: string; const AProps: IProps; const AChildren: array of IMetaElement);
+var
+  mEl: IMetaElement;
+  i: integer;
+begin
+  CheckNotNull(AElement, 'Element is null');
+  CheckEquals(AGuid, AElement.Guid, 'GUID of Element is different');
+  CheckEquals(GUIDToString(AGuid), AElement.TypeGuid, 'Type GUID of Element is different');
+  if AProps = nil then
+    CheckNotNull(AElement.Props, 'Element.Props shouldn''t be nil')
+  else
+    CheckSame(AProps, AElement.Props, 'Element.Props unexpected value');
+  i := 0;
+  for mEl in AElement do
+  begin
+    if i <= High(AChildren) then
+    begin
+      CheckSame(mEl, AChildren[i], 'expeceted child''s element differ from enumerated one');
+    end;
+    inc(i);
+  end;
+  CheckEquals(Length(AChildren), i, 'expected number of children differs from enumerated ones');
+end;
+
+procedure TMetaElementFactoryTests.TestCreateElement_1;
+var
+  mElement: IMetaElement;
+begin
+  mElement := fMetaElementFactory.CreateElement(IDummyObject);
+  CheckMetaElement(mElement, IDummyObject, '', nil, []);
+end;
+
+procedure TMetaElementFactoryTests.TestCreateElement_2;
+var
+  mElement: IMetaElement;
+  mProps: IProps;
+begin
+  mProps := TProps.New;
+  mElement := fMetaElementFactory.CreateElement(IDummyObject, mProps);
+  CheckMetaElement(mElement, IDummyObject, '', mProps, []);
+end;
+
+procedure TMetaElementFactoryTests.TestCreateElement_3;
+var
+  mElement, mEl1, mEl2: IMetaElement;
+begin
+  mElement := fMetaElementFactory.CreateElement(IDummyObject, []);
+  CheckMetaElement(mElement, IDummyObject, '', nil, []);
+  //
+  mEl1 := IMetaElement(DIC.Locate(IMetaElement));
+  mEl2 := IMetaElement(DIC.Locate(IMetaElement));
+  mElement := fMetaElementFactory.CreateElement(IDummyObject, [mEl1, mEl2]);
+  CheckMetaElement(mElement, IDummyObject, '', nil, [mEl1, mEl2]);
+end;
+
+procedure TMetaElementFactoryTests.TestCreateElement_4;
+var
+  mElement, mEl1, mEl2: IMetaElement;
+  mProps: IProps;
+begin
+  mProps := TProps.New;
+  mElement := fMetaElementFactory.CreateElement(IDummyObject, mProps, []);
+  CheckMetaElement(mElement, IDummyObject, '', mProps, []);
+  //
+  mEl1 := IMetaElement(DIC.Locate(IMetaElement));
+  mEl2 := IMetaElement(DIC.Locate(IMetaElement));
+  mElement := fMetaElementFactory.CreateElement(IDummyObject, mProps, [mEl1, mEl2]);
+  CheckMetaElement(mElement, IDummyObject, '', mProps, [mEl1, mEl2]);
+end;
+
+procedure TMetaElementFactoryTests.TestCreateElement_5;
+var
+  mElement: IMetaElement;
+begin
+  mElement := fMetaElementFactory.CreateElement(IDummyObject, cTypeID);
+  CheckMetaElement(mElement, IDummyObject, cTypeID, nil, []);
+end;
+
+procedure TMetaElementFactoryTests.TestCreateElement_6;
+var
+  mElement: IMetaElement;
+  mProps: IProps;
+begin
+  mProps := TProps.New;
+  mElement := fMetaElementFactory.CreateElement(IDummyObject, cTypeID, mProps);
+  CheckMetaElement(mElement, IDummyObject, cTypeID, mProps, []);
+end;
+
+procedure TMetaElementFactoryTests.TestCreateElement_7;
+var
+  mElement, mEl1, mEl2: IMetaElement;
+begin
+  mElement := fMetaElementFactory.CreateElement(IDummyObject, cTypeID, []);
+  CheckMetaElement(mElement, IDummyObject, cTypeID, nil, []);
+  //
+  mEl1 := IMetaElement(DIC.Locate(IMetaElement));
+  mEl2 := IMetaElement(DIC.Locate(IMetaElement));
+  mElement := fMetaElementFactory.CreateElement(IDummyObject, cTypeID, [mEl1, mEl2]);
+  CheckMetaElement(mElement, IDummyObject, cTypeID, nil, [mEl1, mEl2]);
+end;
+
+procedure TMetaElementFactoryTests.TestCreateElement_8;
+var
+  mElement, mEl1, mEl2: IMetaElement;
+  mProps: IProps;
+begin
+  mProps := TProps.New;
+  mElement := fMetaElementFactory.CreateElement(IDummyObject, cTypeID, mProps, []);
+  CheckMetaElement(mElement, IDummyObject, cTypeID, mProps, []);
+  //
+  mEl1 := IMetaElement(DIC.Locate(IMetaElement));
+  mEl2 := IMetaElement(DIC.Locate(IMetaElement));
+  mElement := fMetaElementFactory.CreateElement(IDummyObject, cTypeID, mProps, [mEl1, mEl2]);
+  CheckMetaElement(mElement, IDummyObject, cTypeID, mProps, [mEl1, mEl2]);
+end;
+
+end.
+
--- trl/tests/trltests.lpr
+++ trl/tests/trltests.lpr
@@ -9,13 +9,14 @@ uses
   Interfaces, // this includes the LCL widgetset
   Forms,
   GuiTestRunner,
-  trl_dicontainer_tests, trl_umetaelement_tests;
+  trl_dicontainer_tests, trl_umetaelement_tests, trl_umetaelementfactory_tests;
 
 {$R *.res}
 
 begin
   trl_dicontainer_tests.RegisterTests;
   trl_umetaelement_tests.RegisterTests;
+  trl_umetaelementfactory_tests.RegisterTests;
   RequireDerivedFormResource := True;
   Application.Initialize;
   RunRegisteredTests;
