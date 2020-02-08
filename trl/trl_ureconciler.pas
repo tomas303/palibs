05c16512453dada7fbcaffc94157bf875bfc3276
tomas303 <tomashorak@post.cz>
2020-02-08 20:25:21 +0100

    made reconciler go through all nodes

--- trl/trl_ureconciler.pas
+++ trl/trl_ureconciler.pas
@@ -6,13 +6,18 @@ interface
 
 uses
   trl_ireconciler, trl_ilog, trl_idifactory, trl_iinjector, trl_imetaelement,
-  trl_iprops;
+  trl_iprops, trl_itree, sysutils, Math;
 
 type
 
   { TReconciler }
 
   TReconciler = class(TInterfacedObject, IReconciler)
+  protected
+    procedure RemoveChild(const AParentBit: INode; AIndex: integer);
+    procedure AddChild(const AParentBit: INode; const AChildElement: IMetaElement);
+    procedure ProcessDiff(const ABit: INode; const ANewChildElement, AOldChildElement: IMetaElement);
+    procedure ProcessChildren(const ABit: INode; const AOldElement, ANewElement: IMetaElement);
   protected
     // IReconciler
     function Reconcile(const AOldElement: IMetaElement; const AOldEntity: IUnknown; const ANewElement: IMetaElement): IUnknown;
@@ -30,15 +35,101 @@ implementation
 
 { TReconciler }
 
+procedure TReconciler.RemoveChild(const AParentBit: INode; AIndex: integer);
+var
+  mChildBit: INode;
+begin
+  mChildBit := (AParentBit as INode).Child[AIndex] as INode;
+  (AParentBit as INode).RemoveChild(mChildBit as INode);
+end;
+
+procedure TReconciler.AddChild(const AParentBit: INode;
+  const AChildElement: IMetaElement);
+var
+  mNew: IUnknown;
+  mChildBit: INode;
+  i: integer;
+begin
+  mNew := IUnknown(Factory.Locate(AChildElement.Guid, AChildElement.TypeID, AChildElement.Props.Clone));
+  if Supports(mNew, INode, mChildBit) then
+  begin
+    // what render to INode will be use directly
+    (AParentBit as INode).AddChild(mChildBit as INode);
+    for i := 0 to (AChildElement as INode).Count - 1 do
+      AddChild(mChildBit, (AChildElement as INode).Child[i] as IMetaElement);
+  end
+  else
+  begin
+    raise exception.create('todo');
+  end;
+end;
+
+procedure TReconciler.ProcessDiff(const ABit: INode; const ANewChildElement,
+  AOldChildElement: IMetaElement);
+var
+  mDiffProps: IProps;
+begin
+  mDiffProps := ANewChildElement.Props.Diff(AOldChildElement.Props);
+  if mDiffProps.Count > 0 then
+  begin
+    Injector.Write(ABit as TObject, mDiffProps);
+  end;
+  ProcessChildren(ABit, AOldChildElement, ANewChildElement);
+end;
+
+procedure TReconciler.ProcessChildren(const ABit: INode; const AOldElement,
+  ANewElement: IMetaElement);
+var
+  mOldChildEl: IMetaElement;
+  mNewChildEl: IMetaElement;
+  i: Integer;
+begin
+  Log.DebugLnEnter({$I %CURRENTROUTINE%});
+  for i := 0 to Min((AOldElement as INode).Count, (ANewElement as INode).Count) - 1 do
+  begin
+    mOldChildEl := (AOldElement as INode).Child[i] as IMetaElement;
+    mNewChildEl := (ANewElement as INode).Child[i] as IMetaElement;
+    if (mOldChildEl.TypeGuid <> mNewChildEl.TypeGuid) or (mOldChildEl.TypeID <> mNewChildEl.TypeID) then
+    begin
+      // change of type
+      RemoveChild(ABit, i);
+      AddChild(ABit, mNewChildEl);
+    end
+    else
+    begin
+      // same type - process difference
+      ProcessDiff(ABit.Child[i], mNewChildEl, mOldChildEl);
+    end;
+  end;
+  if (ANewElement as INode).Count > (AOldElement as INode).Count then
+  begin
+    // add new ones
+    for i := (AOldElement as INode).Count to (ANewElement as INode).Count - 1 do
+    begin
+      mNewChildEl := (ANewElement as INode).Child[i] as IMetaElement;
+      AddChild(ABit, mNewChildEl);
+    end;
+  end
+  else
+  begin
+    // remove old ones
+    for i := (ANewElement as INode).Count to (AOldElement as INode).Count - 1 do
+    begin
+      RemoveChild(ABit, (ANewElement as INode).Count);
+    end;
+  end;
+  Log.DebugLnExit({$I %CURRENTROUTINE%});
+end;
+
 function TReconciler.Reconcile(const AOldElement: IMetaElement;
   const AOldEntity: IUnknown; const ANewElement: IMetaElement): IUnknown;
 var
-  mDiffProps: IProps;
+  i: integer;
 begin
   Log.DebugLnEnter({$I %CURRENTROUTINE%});
   if (AOldElement = nil) and (ANewElement = nil) then begin
     Log.DebugLn('both nil');
-    Result := nil;
+    Result := AOldEntity;
   end else
   if (AOldElement <> nil) and (ANewElement = nil) then begin
     Log.DebugLn(AOldElement.TypeGuid + '.' + AOldElement.TypeID + ' to nil');
@@ -46,16 +137,19 @@ begin
   end else
   if (AOldElement = nil) and (ANewElement <> nil) then begin
     Log.DebugLn('from nil to ' + ANewElement.TypeGuid + '.' + ANewElement.TypeID);
-    Result := IUnknown(Factory.Locate(ANewElement.Guid, ANewElement.TypeID));
+    Result := IUnknown(Factory.Locate(ANewElement.Guid, ANewElement.TypeID, ANewElement.Props.Clone));
+    for i := 0 to (ANewElement as INode).Count - 1 do
+      AddChild(Result as INode, (ANewElement as INode).Child[i] as IMetaElement);
   end else
   if (AOldElement.TypeGuid <> ANewElement.TypeGuid) or (AOldElement.TypeID <> ANewElement.TypeID) then begin
     Log.DebugLn('from ' + AOldElement.TypeGuid + '.' + AOldElement.TypeID + ' to ' + ANewElement.TypeGuid + '.' + ANewElement.TypeID);
-    Result := IUnknown(Factory.Locate(ANewElement.Guid, ANewElement.TypeID));
+    Result := IUnknown(Factory.Locate(ANewElement.Guid, ANewElement.TypeID, ANewElement.Props.Clone));
+    for i := 0 to (ANewElement as INode).Count - 1 do
+      AddChild(Result as INode, (ANewElement as INode).Child[i] as IMetaElement);
   end else begin
     Log.DebugLn('equalize props');
-    mDiffProps := ANewElement.Props.Diff(AOldElement.Props);
-    Injector.Write(AOldEntity as TObject, mDiffProps);
     Result := AOldEntity;
+    ProcessDiff(Result as INode, AOldElement, ANewElement);
   end;
   Log.DebugLnExit({$I %CURRENTROUTINE%});
 end;
