c55167623feac1434825e74e5a9b07b1b9504534
tomas303 <tomashorak@post.cz>
2022-12-08 20:57:22 +0100

    add timer back

--- tvl/tvl_utimer.pas
+++ tvl/tvl_utimer.pas
@@ -1,92 +1,121 @@
 unit tvl_utimer;
 
-{$mode objfpc}{$H+}
+{$mode delphi}{$H+}
 
 interface
 
 uses
-  //customtimer
+  customtimer, fgl, sysutils,
   tvl_itimer, InterfaceBase;
 
-//type
-//
-//  { TTimer }
-//
-//  TTimer = class(TInterfacedObject, ITimer)
-//  protected
-//    // ITimer
-//    function GetEnabled: Boolean;
-//    procedure SetEnabled(AValue: Boolean);
-//    property Enabled: Boolean read GetEnabled write SetEnabled;
-//  protected
-//    fHandle: THandle;
-//    fEnabled: Boolean;
-//    procedure TimerHandler;
-//    procedure KillTimer;
-//    procedure UpdateTimer;
-//  public
-//    destructor Destroy; override;
-//  protected
-//    fInterval: integer;
-//    fNotifier: IFluxNotifier;
-//    procedure SetInterval(AValue: integer);
-//    procedure SetNotifier(AValue: IFluxNotifier);
-//  published
-//    property Interval: integer read fInterval write SetInterval;
-//    property Notifier: IFluxNotifier read fNotifier write SetNotifier;
-//  end;
+type
+
+  { TTimer }
+
+  TTimer = class(TInterfacedObject, ITimer)
+  private
+    fObservers: TFPGList<TTimerEvent>;
+  protected
+    // ITimer
+    function GetEnabled: Boolean;
+    procedure SetEnabled(AValue: Boolean);
+    property Enabled: Boolean read GetEnabled write SetEnabled;
+    procedure Subscribe(ACallback: TTimerEvent);
+    procedure Unsubscribe(ACallback: TTimerEvent);
+    procedure Restart;
+  protected
+    fHandle: THandle;
+    fEnabled: Boolean;
+    procedure TimerHandler;
+    procedure KillTimer;
+    procedure UpdateTimer;
+  public
+    destructor Destroy; override;
+    procedure AfterConstruction; override;
+    procedure BeforeDestruction; override;
+  protected
+    fInterval: integer;
+    procedure SetInterval(AValue: integer);
+  published
+    property Interval: integer read fInterval write SetInterval;
+  end;
 
 implementation
 
-//{ TTimer }
-//
-//procedure TTimer.SetInterval(AValue: integer);
-//begin
-//  if fInterval = AValue then Exit;
-//  fInterval := AValue;
-//  UpdateTimer;
-//end;
-//
-//procedure TTimer.SetNotifier(AValue: IFluxNotifier);
-//begin
-//  if fNotifier = AValue then Exit;
-//  fNotifier := AValue;
-//  UpdateTimer;
-//end;
-//
-//function TTimer.GetEnabled: Boolean;
-//begin
-//  Result := fEnabled;
-//end;
-//
-//procedure TTimer.SetEnabled(AValue: Boolean);
-//begin
-//  fEnabled := AValue;
-//end;
-//
-//procedure TTimer.TimerHandler;
-//begin
-//  if Enabled and Assigned(Notifier) then
-//    Notifier.Notify;
-//end;
-//
-//procedure TTimer.KillTimer;
-//begin
-//  if fHandle <> 0 then
-//    WidgetSet.DestroyTimer(fHandle);
-//end;
-//
-//procedure TTimer.UpdateTimer;
-//begin
-//  KillTimer;
-//  fHandle := WidgetSet.CreateTimer(fInterval, @TimerHandler);
-//end;
-//
-//destructor TTimer.Destroy;
-//begin
-//  KillTimer;
-//  inherited Destroy;
-//end;
+{ TTimer }
+
+procedure TTimer.SetInterval(AValue: integer);
+begin
+  if fInterval = AValue then Exit;
+  fInterval := AValue;
+  UpdateTimer;
+end;
+
+function TTimer.GetEnabled: Boolean;
+begin
+  Result := fEnabled;
+end;
+
+procedure TTimer.SetEnabled(AValue: Boolean);
+begin
+  fEnabled := AValue;
+end;
+
+procedure TTimer.Subscribe(ACallback: TTimerEvent);
+begin
+  if fObservers.IndexOf(ACallback) = -1 then
+    fObservers.Add(ACallback);
+end;
+
+procedure TTimer.Unsubscribe(ACallback: TTimerEvent);
+begin
+  if fObservers.IndexOf(ACallback) <> -1 then
+    fObservers.Remove(ACallback);
+end;
+
+procedure TTimer.Restart;
+begin
+  UpdateTimer;
+end;
+
+procedure TTimer.TimerHandler;
+var
+  o: TTimerEvent;
+begin
+  if Enabled then
+    for o in fObservers do
+      o();
+end;
+
+procedure TTimer.KillTimer;
+begin
+  if fHandle <> 0 then
+    WidgetSet.DestroyTimer(fHandle);
+end;
+
+procedure TTimer.UpdateTimer;
+begin
+  KillTimer;
+  fHandle := WidgetSet.CreateTimer(fInterval, TimerHandler);
+end;
+
+destructor TTimer.Destroy;
+begin
+  KillTimer;
+  inherited Destroy;
+end;
+
+procedure TTimer.AfterConstruction;
+begin
+  inherited AfterConstruction;
+  fObservers := TFPGList<TTimerEvent>.Create;
+end;
+
+procedure TTimer.BeforeDestruction;
+begin
+  FreeAndNil(fObservers);
+  inherited BeforeDestruction;
+end;
 
 end.
 
